c39037b1a7ab18f9ffaf7fc01d7ab2f88ed19308
Enzo Bustamante <enzobustamante895@gmail.com>
2024-10-26 20:44:59 -0300

    feat: finish social.media demo

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,13 +1,13 @@
 export const FeedData = [
   {
     id: 1,
-    name: "Deku",
-    username: "@deku123",
+    name: "Hayao Miyazaki",
+    username: "@hayao_miyazaki",
     hour: "1h",
     profile:
-      "https://i.pinimg.com/564x/59/ee/a9/59eea9917a0f38bb84f0aefb2fcd05a6.jpg",
+      "https://i.pinimg.com/564x/8b/bb/bf/8bbbbfd842483a338b7142ee87874d0d.jpg",
     image: "",
-    post: "lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt facilis esse vero, exercitationem delectus molestias corrupti quaerat incidunt, corporis quasi dolores, ut autem suscipit?",
+    post: "La vida es un viaje, no un destino.",
     like: "584",
     comment: "12",
     share: "2",
@@ -15,18 +15,106 @@ export const FeedData = [
   },
   {
     id: 2,
-    name: "Naruto",
-    username: "@naruto123",
+    name: "Quentin Tarantino",
+    username: "@tarantino_fan",
     hour: "8h",
     profile:
-      "https://i.pinimg.com/564x/59/ee/a9/59eea9917a0f38bb84f0aefb2fcd05a6.jpg",
+      "https://i.pinimg.com/236x/81/e6/09/81e60981dc1f7909fa0730d51717a1ff.jpg",
     image:
-      "https://i.pinimg.com/564x/59/ee/a9/59eea9917a0f38bb84f0aefb2fcd05a6.jpg",
-
-    post: "lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt facilis esse vero, exercitationem delectus molestias corrupti quaerat incidunt, corporis quasi dolores, ut autem suscipit?",
+      "https://i.pinimg.com/236x/a1/71/4d/a1714d544a5bcff74b3062c7139543ff.jpg",
+    post: "El cine es una forma de arte que puede cambiar la vida.",
     like: "1.254",
     comment: "87",
     share: "34",
     bookmarks: "55",
+  },
+  {
+    id: 3,
+    name: "Alfred Hitchcock",
+    username: "@hitchcock_master",
+    hour: "2h",
+    profile:
+      "https://i.pinimg.com/236x/38/5c/3e/385c3ea487052a6e4e4329c25b1eb7c2.jpg",
+    image:
+      "https://i.pinimg.com/236x/7e/9c/98/7e9c98cdd5d61da9a527d29c3e682620.jpg",
+    post: "La tensión es el corazón del cine.",
+    like: "1.500",
+    comment: "45",
+    share: "10",
+    bookmarks: "20",
+  },
+  {
+    id: 4,
+    name: "Martin Scorsese",
+    username: "@martin_scorsese",
+    hour: "3h",
+    profile:
+      "https://i.pinimg.com/236x/a0/d0/0e/a0d00e0b216c18bdaabee426c119c409.jpg",
+    image:
+      "https://i.pinimg.com/236x/c4/2f/53/c42f536fe9d1e73a9c3f4d80475bd92a.jpg",
+    post: "El cine es un espejo de la sociedad.",
+    like: "900",
+    comment: "30",
+    share: "15",
+    bookmarks: "25",
+  },
+  {
+    id: 5,
+    name: "Stanley Kubrick",
+    username: "@kubrick_legend",
+    hour: "5h",
+    profile:
+      "https://i.pinimg.com/236x/52/9b/65/529b65af0f819de525b2ca0d4104a531.jpg",
+    image:
+      "https://i.pinimg.com/236x/ca/d9/f5/cad9f55ad0dd07570a4a27582a3558a3.jpg",
+    post: "La perfección es un objetivo inalcanzable.",
+    like: "2.000",
+    comment: "60",
+    share: "25",
+    bookmarks: "40",
+  },
+  {
+    id: 6,
+    name: "James Mangold",
+    username: "@mangold_fanatic",
+    hour: "4h",
+    profile:
+      "https://i.pinimg.com/736x/dd/e4/0b/dde40bb06641ad37c0b09ee047902616.jpg",
+    image:
+      "https://i.pinimg.com/236x/96/57/26/9657261c4b3797841f1ac98f7b1b7d67.jpg",
+    post: "La imaginación es más importante que el conocimiento.",
+    like: "750",
+    comment: "20",
+    share: "5",
+    bookmarks: "10",
+  },
+  {
+    id: 7,
+    name: "John Carpenter",
+    username: "@TheHorrorMaster",
+    hour: "6h",
+    profile:
+      "https://pbs.twimg.com/profile_images/3605739295/3b263103307763f948f3e1fd494a095e_400x400.jpeg",
+    image:
+      "https://i.pinimg.com/236x/f2/ab/8a/f2ab8a9aead96217d2d3369192f794e2.jpg",
+    post: "El cine debe ser como un sueño.",
+    like: "500",
+    comment: "15",
+    share: "10",
+    bookmarks: "20",
+  },
+  {
+    id: 8,
+    name: "Akira Kurosawa",
+    username: "@Rashōmon_fan_account",
+    hour: "7h",
+    profile:
+      "https://i.pinimg.com/236x/29/07/42/2907426f7a14eeb8c841883c42c49a74.jpg",
+    image: "",
+    post: "El cine es una forma de expresión personal.",
+    like: "800",
+    comment: "25",
+    share: "12",
+    bookmarks: "18",
   },
 ];
