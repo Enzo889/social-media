5f55614936827b5bc550dfd143d2f83d5607cd6e
Enzo Bustamante <enzobustamante895@gmail.com>
2024-10-26 01:05:07 -0300

    feat: add constants and feed data

---
+++ src/constants/data.ts
@@ -0,0 +1,32 @@
+export const FeedData = [
+  {
+    id: 1,
+    name: "Deku",
+    username: "@deku123",
+    hour: "1h",
+    profile:
+      "https://i.pinimg.com/564x/59/ee/a9/59eea9917a0f38bb84f0aefb2fcd05a6.jpg",
+    image: "",
+    post: "lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt facilis esse vero, exercitationem delectus molestias corrupti quaerat incidunt, corporis quasi dolores, ut autem suscipit?",
+    like: "584",
+    comment: "12",
+    share: "2",
+    bookmarks: "3",
+  },
+  {
+    id: 2,
+    name: "Naruto",
+    username: "@naruto123",
+    hour: "8h",
+    profile:
+      "https://i.pinimg.com/564x/59/ee/a9/59eea9917a0f38bb84f0aefb2fcd05a6.jpg",
+    image:
+      "https://i.pinimg.com/564x/59/ee/a9/59eea9917a0f38bb84f0aefb2fcd05a6.jpg",
+
+    post: "lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt facilis esse vero, exercitationem delectus molestias corrupti quaerat incidunt, corporis quasi dolores, ut autem suscipit?",
+    like: "1.254",
+    comment: "87",
+    share: "34",
+    bookmarks: "55",
+  },
+];
